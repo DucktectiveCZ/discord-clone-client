671c9aa7e20b87d112111931c6ea37783578de88
fini <jsabew_spam@icloud.com>
2025-06-03 20:42:32 +0200

    added login.js and register.js to start integrating the api

---
+++ login.js
@@ -0,0 +1,27 @@
+const api_url = "http://localhost:8080/api/v1";
+
+function done_button_click() {
+    const username = document.getElementById("input_username").textContent;
+    const password = document.getElementById("input_password").textContent;
+
+    fetch(api_url+"/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json" 
+        },
+        body: JSON.stringify({username:username, password:password})
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Uh oh, the duck couldn’t fly :[");
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log("Yay! Server got the duck!", data);
+      })
+      .catch(error => {
+        console.error("Something went wrong:", error);
+        alert("Oh nooo the duck got lost!");
+      });
+}
