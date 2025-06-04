3fa895e18a2f1005be0396af0e4c39bc23eab73c
fini <jsabew_spam@icloud.com>
2025-06-04 19:40:15 +0200

    added api.js -> moved the register and login function there, modified stuff, idk i did thingy

---
+++ api.js
@@ -0,0 +1,47 @@
+const api_url = "http://localhost:8080/api/v1"
+
+export function register(username, password, email, displayName) {
+    fetch(api_url+"/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json" 
+        },
+        body: JSON.stringify({username:username, password:password})
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Uh oh, the duck couldn’t fly :[");
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log("Yay! Server got the duck!", data);
+      })
+      .catch(error => {
+        console.error("Something went wrong:", error);
+        alert("Oh nooo the duck got lost!");
+      });
+}
+
+export function login(username, password) {
+    fetch(api_url+"/login", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json" 
+        },
+     body: JSON.stringify({username:username, password:password})
+     })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Uh oh, the duck couldn’t fly :[");
+      }
+      return response.json();
+     })
+     .then(data => {
+      console.log("Yay! Server got the duck!", data);
+     })
+     .catch(error => {
+      console.error("Something went wrong:", error);
+     alert("Oh nooo the duck got lost!");
+    });
+}
--- register.js
+++ register.js
@@ -1,29 +1,9 @@
-const api_url = "http://localhost:8080/api/v1";
+import { register } from './api.js';
 
 function done_button_click() {
-    const username = document.getElementById("input_username").textContent;
-    const password = document.getElementById("input_password").textContent;
-    const email = document.getElementById("input_email").textContent;
-    const displayName = document.getElementById("input_displayName").textContent;
-
-    fetch(api_url+"/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json" 
-        },
-        body: JSON.stringify({username:username, password:password})
-      })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error("Uh oh, the duck couldn’t fly :[");
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log("Yay! Server got the duck!", data);
-      })
-      .catch(error => {
-        console.error("Something went wrong:", error);
-        alert("Oh nooo the duck got lost!");
-      });
+  const username = document.getElementById("input_username").textContent;
+  const password = document.getElementById("input_password").textContent;
+  const email = document.getElementById("input_email").textContent;
+  const displayName = document.getElementById("input_displayName").textContent;
+  register(username, password, email, displayName)
 }
